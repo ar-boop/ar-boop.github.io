c859e659eb9d9cc1f38a7ac8fcdd5371b6c50616
ar-boop <83057745+ar-boop@users.noreply.github.com>
2021-04-24 17:03:21 +0530

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,6 +3,6 @@
 	console.log('bookmarklet starting');
 	let paragraphs = document.getElementsByTagName('p');
 	for (let i = 0; i < paragraphs.length; i++) {
-		paragraphs[i].innerHTML = 'Changing Now';
+		paragraphs[i].innerHTML = 'New Change';
 	}
 })();

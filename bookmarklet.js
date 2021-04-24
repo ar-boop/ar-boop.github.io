491bf7e1812ee0603299f17b6ae365a82a24806a
ar-boop <83057745+ar-boop@users.noreply.github.com>
2021-04-24 16:33:02 +0530

    Trying with codeTrain

---
+++ bookmarklet.js
@@ -0,0 +1,8 @@
+(function () {
+	// alert('Check the console');
+	console.log('bookmarklet starting');
+	let paragraphs = document.getElementsByTagName('p');
+	for (let i = 0; i < paragraphs.length; i++) {
+		paragraphs[i].innerHTML = 'kitten';
+	}
+})();
